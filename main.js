891e0ce8609d25d54311d06ea86cee2e530cdb1b
Leonardo Pavlavicius Estigarribia <123014472+Est1ga@users.noreply.github.com>
2023-08-01 10:56:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function tocaSom(param_audio) {
     if (audioEmExecucao != null ) {
         setTimeout(function(){
         audioEmExecucao.pause(); 
-        },2000)
+        },)
         
     }
 
@@ -15,39 +15,16 @@ function tocaSom(param_audio) {
     audioEmExecucao = audio; 
 }
 
-function tocaSom_touch(param_audio) {
 
-    const audio = document.querySelector(param_audio);
-    audio.currentTime = 0; 
-    audio.play();
-    audioEmExecucao = audio; 
-}
-function paraSom_touch() {
-    if (audioEmExecucao != null ) {
-        audioEmExecucao.pause(); 
-        }
-
-
-}
 for (let i = 0; i < lista_tecla.length; i++) {
 
     let touchStarted = false;
 
     lista_tecla[i].addEventListener('touchstart', function () {
         let param_class = this.classList[3];
-        tocaSom_touch(`.som_${param_class}`);
-        touchStarted = true;
-
-      });
-      lista_tecla[i].addEventListener('touchend', function () {
-        setTimeout(function(){
-        paraSom_touch(); 
-        },500)
+        tocaSom(`.som_${param_class}`);
         touchStarted = true;
 
       });
 
-
-
-    
 }
