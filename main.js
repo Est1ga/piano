d1dea6b1742c38b55bc310f3ada08e9debda79f0
Leonardo Pavlavicius Estigarribia <123014472+Est1ga@users.noreply.github.com>
2023-08-01 09:35:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,8 +25,8 @@ function tocaSom_touch(param_audio) {
 function paraSom_touch() {
 if (audioEmExecucao != null) {
   let volume = audioEmExecucao.volume;
-  const fadeOutInterval = 5; // Intervalo de tempo para diminuir o volume em cada etapa (em milissegundos)
-  const fadeOutSteps = 10; // Número de etapas para diminuir o volume
+  const fadeOutInterval = 1; // Intervalo de tempo para diminuir o volume em cada etapa (em milissegundos)
+  const fadeOutSteps = 8; // Número de etapas para diminuir o volume
 
   // Função para diminuir o volume em cada etapa
   function fadeOutVolume() {
